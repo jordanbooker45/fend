b227d920a833e98427cc4fe3b3db91bea187962f
Jordy Bookie <jordan.m.booker@gmail.com>
2020-04-25 03:23:56 -0400

    Add JS to update nav dynamically with li elements

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,23 +17,45 @@
  * Define Global Variables
  * 
 */
-
-
+const sections = document.querySelectorAll('.section-title');
+const navBarList = document.getElementById('navbar__list');
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
 
+function createSectionLi (sectionTitle) {
+    const _sectionTitle = sectionTitle;
+    const newLi = document.createElement('li');
+    newLi.textContent = _sectionTitle;
+    newLi.classList.add('menu__link');
+    navBarList.appendChild(newLi);
+}
 
+function createLink ()
 
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
+// add: onPageLoad call "main function (for..loop)" 
+
+function formatMenu() {
+    for (let section of sections) {
+        let sectionTitle = section.innerText;
+        createSectionLi(sectionTitle);
+
+
+     }
+}
+
+
 
 // build the nav
+
+
 
 
 // Add class 'active' to section when near top of viewport
