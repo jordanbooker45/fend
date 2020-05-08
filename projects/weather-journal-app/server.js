05870e6df210b77486305d5a42318e75c9d81ae5
Jordan Booker <jordan.m.booker@gmail.com>
2020-05-07 22:30:55 -0400

    Added routes and server functionality

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -3,7 +3,13 @@ projectData = {};
 
 // Require Express to run server and routes
 
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 // Start up an instance of app
+
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -12,8 +18,35 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 
-// Initialize the main project folder
-app.use(express.static('website'));
+app.use(cors());
 
+// Initialize the main project folder
+app.use(express.static("website"));
 
 // Setup Server
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}!`));
+
+// Get route
+app.get("/", getData);
+
+function getData(req, res) {
+  res.send(projectData);
+  console.log(projectData);
+}
+
+// Post route
+
+app.post("/", postData);
+
+function postData(req, res) {
+  let newData = request.body;
+  let newEntry = {
+    temp: newData.temp,
+    date: newData.date,
+    userResponse: newData.user - response,
+  };
+  projectData.push(newEntry);
+}
