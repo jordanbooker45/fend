595e3444fafc37b24a7b72cfb594898a1a7fd7a4
Jordan Booker <jordan.m.booker@gmail.com>
2020-05-10 17:35:29 -0400

    Added weather api to app and fixed routes

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,11 +1,12 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = { name: "Wassup" };
 
 // Require Express to run server and routes
 
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
 // Start up an instance of app
 
@@ -30,23 +31,27 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}!`));
 
 // Get route
-app.get("/", getData);
+app.get("/api", getData);
 
 function getData(req, res) {
   res.send(projectData);
-  console.log(projectData);
 }
 
 // Post route
 
-app.post("/", postData);
+app.post("/api", postData);
 
 function postData(req, res) {
-  let newData = request.body;
+  let newData = req.body;
   let newEntry = {
     temp: newData.temp,
     date: newData.date,
-    userResponse: newData.user - response,
+    userResponse: newData.content,
   };
   projectData.push(newEntry);
 }
+
+// Bring in API key
+
+dotenv.config();
+console.log("the value of the api is: ", process.env.APIKEY);
